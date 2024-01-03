33477298c09c51e6e981d58629c1168a7ca45645
2061360308 <2061360308@qq.com>
2024-01-03 18:22:03 +0800

    fix:同步最近的更新内容

--- 玻璃相册/wallPaper.py
+++ 玻璃相册/wallPaper.py
@@ -1,10 +1,5 @@
 import ctypes,sys
 
-```
-通过修改注册表来更改壁纸, 接受一个命令行参数
-具体代码找不到了,原理如下:
-```
-
 class Main:
     def __init__(self):
         path = 'D:\卢澳\Pictures\飞火壁纸库\FFWallPaper\custom\img\ef93148c2df9ca39b3a3ddbdb6171b22.jpg'
